c190a659b055c54e2d04c444a8f1c9bcf55360d2
SteamShon <shom83@gmail.com>
2015-11-26 10:37:11 +0900

    change redis client

--- s2core/src/main/scala/com/kakao/s2graph/core/cache/RedisCache.scala
+++ s2core/src/main/scala/com/kakao/s2graph/core/cache/RedisCache.scala
@@ -3,18 +3,35 @@ package com.kakao.s2graph.core.cache
 
 import java.util.concurrent.TimeUnit
 
+import akka.util.ByteString
 import com.google.common.cache.CacheBuilder
 import com.kakao.s2graph.core.storage.hbase.{AsynchbaseStorage, AsynchbaseQueryBuilder}
-import com.kakao.s2graph.core.{QueryRequest, QueryResult}
+import com.kakao.s2graph.core.utils.logger
+import com.kakao.s2graph.core.{GraphUtil, QueryRequest, QueryResult}
 import com.kakao.s2graph.core.storage.{SCache}
 import com.typesafe.config.Config
 import org.hbase.async.GetRequest
+import redis.RedisClient
 import scala.concurrent.{Promise, Future, ExecutionContext}
+import scala.collection.JavaConversions._
+import scala.util.{Failure, Success}
 
 class RedisCache(config: Config, storage: AsynchbaseStorage)(implicit ec: ExecutionContext)
   extends SCache[QueryRequest, Future[Seq[QueryResult]]] {
 
-  val clients = new WithRedis(config)
+  implicit val akkaSystem = akka.actor.ActorSystem()
+
+  val instances = if (config.hasPath("redis.instances")) config.getStringList("redis.instances").toList else List("localhost")
+  val database = if (config.hasPath("redis.database")) config.getInt("redis.database") else 0
+  val clients = instances map { s =>
+    val sp = s.split(':')
+    val (host, port) = if (sp.length > 1) (sp(0), sp(1).toInt) else (sp(0), 6379)
+    RedisClient(host = host, port = port, db = Option(database))
+  } toVector
+  val poolSize = clients.size
+
+  private def shardKey(key: Any): Int = GraphUtil.murmur3Int(key.toString) % poolSize
+  def getClient(key: Any): RedisClient = clients.get(shardKey(key))
 
   val builder = new AsynchbaseQueryBuilder(storage)
 
@@ -36,19 +53,23 @@ class RedisCache(config: Config, storage: AsynchbaseStorage)(implicit ec: Execut
     val promise = Promise[Seq[QueryResult]]
     cache.asMap().putIfAbsent(key, promise.future) match {
       case null =>
-        val future = Future {
-          clients.doBlockWithKey(key.toString) { jedis =>
-            val v = jedis.get(getBytes(key))
-            if (v == null) Nil
-            else {
-              QueryResult.fromBytes(storage, queryRequest)(v, 0)
-            }
+        val future = getClient(key).get(key.toString).map { valueOpt =>
+          valueOpt match {
+            case None => Nil
+            case Some(ls) => QueryResult.fromBytes(storage, queryRequest)(ls.toArray, 0)
           }
         }
-        future.onComplete { value =>
-          promise.complete(value)
-          if (value.isFailure) cache.asMap().remove(key, promise.future)
+        future onComplete {
+          case Success(value) => promise.success(value)
+          case Failure(ex) =>
+            logger.error(s"getIfPresent failed.")
+            cache.asMap().remove(key, promise.future)
         }
+//        future.onComplete { valueOpt =>
+//          promise.complete(valueOpt)
+//          if (valueOpt.isFailure) cache.asMap().remove(key, promise.future)
+//        }
+
         future
       case existingFuture => existingFuture
     }
@@ -61,9 +82,10 @@ class RedisCache(config: Config, storage: AsynchbaseStorage)(implicit ec: Execut
         val key = toCacheKey(queryRequest)
         val queryResultLs = queryResultLsTry.get
         val bytes = QueryResult.toBytes(storage)(queryResultLs)
-        clients.doBlockWithKey(key.toString) { jedis =>
-          jedis.setex(getBytes(key), toTs(queryRequest), bytes)
-        }
+        getClient(key).setex(key.toString, toTs(queryRequest), ByteString(bytes))
+//        clients.doBlockWithKey(key.toString) { jedis =>
+//          jedis.setex(getBytes(key), toTs(queryRequest), bytes)
+//        }
       }
     }
   }
